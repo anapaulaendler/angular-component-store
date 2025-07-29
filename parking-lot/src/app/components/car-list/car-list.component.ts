449217d2fff872066d13b9085f30db9259c5e036
ana paula <ana.endler@headsoft.com.br>
2025-07-29 15:32:59 -0300

    feature: car-list component

---
+++ parking-lot/src/app/components/car-list/car-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Car } from 'src/app/models/car';
+
+@Component({
+  selector: 'app-car-list',
+  templateUrl: './car-list.component.html',
+  styleUrls: ['./car-list.component.scss']
+})
+export class CarListComponent implements OnInit {
+  @Input() cars: Car[] = []
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
