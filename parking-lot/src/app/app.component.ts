58b55b0b5730a049a1480fa376dc2b80a1efb253
ana paula <ana.endler@headsoft.com.br>
2025-07-29 16:11:24 -0300

    consuming the store service

--- parking-lot/src/app/app.component.ts
+++ parking-lot/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { StoreService } from './store/store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  /** 
+   * You inject the service at the component level so that this instance only has 
+   * this component and its children.
+   */
+  providers: [StoreService],
 })
 export class AppComponent {
-  title = 'parking-lot';
+  plate = ''
+  vm$ = this.store.vm$
+
+  constructor(private store: StoreService) {}
+
+  onSubmit($event: Event) {
+    $event.preventDefault()
+    this.store.addCarToParkingLot(this.plate)
+    this.plate = "";
+  }
+
+  addPlate($event: Event) {
+    const target = $event.target as HTMLButtonElement // TODO: rever aqui tambem a tipagem nativa do ts
+
+    if (target.nodeName === 'BUTTON') {
+      this.plate = target.innerHTML
+    }
+  }
 }
--- parking-lot/src/app/app.module.ts
+++ parking-lot/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarListComponent } from './components/car-list/car-list.component';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CarListComponent } from './components/car-list/car-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
